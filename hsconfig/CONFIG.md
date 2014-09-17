ab3270bb6404333ecf98ded20b5bc11597eb7d83
Jonas Bengtson <jonas@jonasbengtson.se>
2014-09-17 11:07:03 +0200

    OK

--- hsconfig/CONFIG.md
+++ hsconfig/CONFIG.md
@@ -2,10 +2,17 @@
 
 ADD log.config to:
 
-Windows: C:\Users\YOURUSER\AppData\Local\Blizzard\Hearthstone or just %LOCALAPPDATA%/Blizzard/Hearthstone
-Mac: ~/Library/Preferences/Blizzard/Hearthstone
+
+
+###Windows: C:\Users\YOURUSER\AppData\Local\Blizzard\Hearthstone or just %LOCALAPPDATA%/Blizzard/Hearthstone
+
+###Mac: ~/Library/Preferences/Blizzard/Hearthstone
+
+
 
 READ THE SHITTY LOG:
 
-Windows: C:\Program Files (x86)\Hearthstone\Hearthstone_Data\output_log.txt
-Mac: /Users/YOURUSER/Library/Logs/Unity/Player.log
+
+###Windows: C:\Program Files (x86)\Hearthstone\Hearthstone_Data\output_log.txt
+
+###Mac: /Users/YOURUSER/Library/Logs/Unity/Player.log
--- index.js
+++ index.js
@@ -1,25 +1,45 @@
+var express = require('express');
+var app = express();
+var socket = require('socket.io-client')('http://127.0.0.1:3001');
 var spawn = require('child_process').spawn;
 
-tail = spawn('tail', ['-f', '/Users/coral/Library/Logs/Unity/Player.log']);
+var tail = spawn('tail', ['-f', '/Users/coral/Library/Logs/Unity/Player.log']);
 
-tail.stdout.on('data', function (data) {
-  //console.log(String(data).indexOf('\n'));
+var turn = true;
 
-  var str = String(data).split('\n');
+//socket.on('connect', function(){
+  tail.stdout.on('data', function (data) {
+    //console.log(String(data).indexOf('\n'));
 
-  for(var i in str)
-  {
-  	//console.log(str[i]);
-  	temp = str[i];
-  	if(temp.indexOf("END waiting for zone FRIENDLY DECK") != -1)
-  	{
-  		console.log(temp);
-  	}
+    var str = String(data).split('\n');
 
-  	if(temp.indexOf("END waiting for zone OPPOSING DECK") != -1)
-  	{
-  		console.log(temp);
-  	}
-  }
-  
+    for(var i in str)
+    {
+    	//console.log(str[i]);
+    	temp = str[i];
+    	if(temp.indexOf("END waiting for zone FRIENDLY DECK") != -1)
+    	{
+    		console.log("MY TURN");
+        turn = true;
+        //socket.emit("event", turn);
+    	}
+
+    	if(temp.indexOf("END waiting for zone OPPOSING DECK") != -1)
+    	{
+    		console.log("NOT MY TURN");
+        turn = false;
+        //socket.emit("event", turn);
+    	}
+    }
+    
+  });
+//});
+
+
+app.get('*', function(req, res){
+  res.send(turn);
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
 });
